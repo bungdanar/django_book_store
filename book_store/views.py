4dc8ba9f7c382f3856428f70f44bb74125fcf02c
Bung Danar <danar.g.putera@gmail.com>
2021-05-14 21:16:49 +0700

    End of Section 6

--- book_store/views.py
+++ book_store/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpRequest, Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpRequest
+from django.db.models import Avg
 
 from .models import Book
 
 
 def index(request: HttpRequest):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
     return render(request, "book_store/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
     })
 
 
-def book_detail(request: HttpRequest, id: int):
-    try:
-        book: Book = Book.objects.get(pk=id)
-        return render(request, "book_store/book_detail.html", {
-            "title": book.title,
-            "author": book.author,
-            "rating": book.rating,
-            "is_bestselling": book.is_bestselling
-        })
-    except:
-        raise Http404()
+def book_detail(request: HttpRequest, slug: str):
+    book: Book = get_object_or_404(Book, slug=slug)
+    return render(request, "book_store/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+    })
