5964aef4d8cc99b4eb1b73bebc9b29b8b5c90eb4
Bung Danar <danar.g.putera@gmail.com>
2021-05-14 18:45:58 +0700

    Section 6: Model URLs

--- book_store/views.py
+++ book_store/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpRequest, Http404
 
-# Create your views here.
+from .models import Book
+
+
+def index(request: HttpRequest):
+    books = Book.objects.all()
+    return render(request, "book_store/index.html", {
+        "books": books
+    })
+
+
+def book_detail(request: HttpRequest, id: int):
+    try:
+        book: Book = Book.objects.get(pk=id)
+        return render(request, "book_store/book_detail.html", {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "is_bestselling": book.is_bestselling
+        })
+    except:
+        raise Http404()
